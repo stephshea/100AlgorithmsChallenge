d51ac54445b079ba6c3559b24b858e7b20294877
Stephanie <stephcshea@gmail.com>
2018-06-30 10:30:27 -0400

    completed add

--- add/add.js
+++ add/add.js
@@ -1,8 +1,25 @@
 function add(param1, param2) {
+  return param1 + param2;
 }
-// function add2(param1: number[]): number {
+
+function add2(...param1) {
+  var result = 0;
+  param1.forEach(element => {
+    result += element;
+  }); 
+  return result;
+  }
+
+// function add2(param1) {
+//   var result = 0;
+//   for(var i = 0; i < arguments.length; i++){
+    
+//   result +=arguments[i];
+//   }
+// return result;
 // }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+
+console.log(add(1, 2));
+console.log(add(3, 2));
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,17 @@
 function add(param1: number, param2: number): number {
-
+return param1 + param2;
 }
 
-// function add2(param1: number[]): number {
+function add2(...param1: number[]): number {
+  let result = 0;
+  param1.forEach((element) => {
+    result += element;
+  });
+  return result;
+  }
 
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+console.log(add(1, 2));
+console.log(add(3, 2));
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
-
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
