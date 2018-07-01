fc72c0559ae4fd3ff821193dfb79ee0fceb516df
Stephanie <stephcshea@gmail.com>
2018-07-01 15:43:19 -0400

    updating

--- alphabeticShift/alphabeticShift.js
+++ alphabeticShift/alphabeticShift.js
@@ -1,19 +1,13 @@
 function alphabeticShift(inputString) {
-    var alphabet = {
-        'a': 'b', 'b': 'c', 'c': 'd',
-        'd': 'e', 'e': 'f', 'f': 'g',
-        'g': 'h', 'h': 'i', 'i': 'j',
-        'j': 'k', 'k': 'l', 'l': 'm',
-        'm': 'n', 'n': 'o', 'o': 'p',
-        'p': 'q', 'q': 'r', 'r': 's',
-        's': 't', 't': 'u', 'u': 'v',
-        'v': 'w', 'w': 'y', 'y': 'z',
-        'z': 'a'
-    };
-    var inputShifted = inputString.split('');
-    for (var i = 0; i < inputShifted.length; i++) {
-        inputShifted[i] = alphabet[inputShifted[i]];
-    }
-    return inputShifted.join('');
+    
+    // var inputShifted = inputString.split('');
+    newString = inputString.split("");
+    for (var i = 0; i < newString.length; i++) {
+      
+       var newAb = String.fromCharCode(newString.charCodeAt(i) +1);
+var newablist= newablist.push(newAb);
+    } 
+    // return inputShifted.join('');
+    return newablist;
 }
 console.log(alphabeticShift('crazy'));
