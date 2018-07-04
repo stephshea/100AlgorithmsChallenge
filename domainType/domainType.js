d105a6682b88763a8fa87e31398157999dc715bb
Stephanie <stephcshea@gmail.com>
2018-07-04 07:35:12 -0400

    completed domainType

--- domainType/domainType.js
+++ domainType/domainType.js
@@ -1,3 +1,30 @@
 function domainType(domains) {
+  
+  let terms = {
+    "org" : "organization",
+    "com": "commercial",
+    "net" : "network",
+    "info" : "information"
+  }
+
+let domainTypes = [];
+
+for (let i = 0; i < domains.length; i++) {
+  if (domains[i].includes("org")) {
+    domainTypes.push("organization");
+    }
+  if (domains[i].includes("com")) {
+    domainTypes.push("commercial");
+    }
+  if (domains[i].includes("net")) {
+    domainTypes.push("network");
+    }
+  if (domains[i].includes("info")) {
+    domainTypes.push("information");
+    }
+  }
+   return domainTypes;
 }
+
 console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+
--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,21 @@
 function domainType(domains: string[]): string[] {
+const domainTypes: string [] = [];
 
+for (let i = 0; i < domains.length; i++) {
+  if (domains[i].includes("org")) {
+    domainTypes.push("organization");
+    }
+  if (domains[i].includes("com")) {
+    domainTypes.push("commercial");
+    }
+  if (domains[i].includes("net")) {
+    domainTypes.push("network");
+    }
+  if (domains[i].includes("info")) {
+    domainTypes.push("information");
+    }
+  }
+return domainTypes;
 }
 
 console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
