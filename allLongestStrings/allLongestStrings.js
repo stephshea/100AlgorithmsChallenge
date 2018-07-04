76b6406b19e2fd13b4f5eb234022d0e535789adf
Stephanie <stephcshea@gmail.com>
2018-07-04 16:50:22 -0400

    completed allLongestStrings

--- allLongestStrings/allLongestStrings.js
+++ allLongestStrings/allLongestStrings.js
@@ -1,4 +1,14 @@
 function allLongestStrings(inputArray) {
-  
+  let sorted = inputArray.sort(function(a,b) {
+    return b.length - a.length;
+  });
+  let sub = sorted[0].length;
+  let longArray = [];
+  inputArray.forEach((item) => {
+    if(item.length === sub) {
+      longArray.push(item);
+    }
+  });
+return longArray;
 }
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,16 @@
 function allLongestStrings(inputArray: string[]): string[] {
-   
+  const sorted = inputArray.sort(function (a, b) {
+    return b.length - a.length;
+  });
+  const sub: number = sorted[0].length;
+  const longArray = [];
+  // inputArray.forEach(function(element) {
+  inputArray.forEach((item: string) => {
+    if (item.length === sub) {
+      longArray.push(item);
+    }
+  });
+  return longArray;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
