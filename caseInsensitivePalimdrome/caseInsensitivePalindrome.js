00699f81a9633db0abd27f2d7ee085997e0eaa37
Stephanie <stephcshea@gmail.com>
2018-07-04 12:17:52 -0400

    Completed isCaseInsensitivePalindrome

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.js
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.js
@@ -1,5 +1,10 @@
 function isCaseInsensitivePalindrome(inputString) {
-  
+    inputString = inputString.toLowerCase();
+    let reverse = inputString.split("").reverse().join("");
+    if(inputString === reverse){
+        return true;
+    }
+    return false;
 }
 console.log(isCaseInsensitivePalindrome('AaBaa'));
 console.log(isCaseInsensitivePalindrome('abac'));
--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,5 +1,7 @@
 function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+  inputString = inputString.toLowerCase();
+  const reverse = inputString.split("").reverse().join("");
+  return inputString === reverse;
 }
 
 console.log(isCaseInsensitivePalindrome('AaBaa'));
