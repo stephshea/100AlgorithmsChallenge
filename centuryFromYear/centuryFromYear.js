4154c3e749d6ca43fb0d81105aa1b378d6d424df
Stephanie <stephcshea@gmail.com>
2018-07-04 12:42:15 -0400

    completed centuryFromYear

--- centuryFromYear/centuryFromYear.js
+++ centuryFromYear/centuryFromYear.js
@@ -1,5 +1,9 @@
 function centuryFromYear(year) {
-   
-}
+    let century = year/100;
+    if(Number.isInteger(century)){
+        return Math.floor(century);
+    }
+    return Math.floor(century) + 1;
+}    
 console.log(centuryFromYear(1905));
 console.log(centuryFromYear(1700));
--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -1,5 +1,10 @@
 function centuryFromYear(year: number): number {
- 
+  const century = year / 100;
+  if (year % 100 === 0)
+  if(Number.isInteger(century)){
+      return Math.floor(century);
+  }
+  return Math.floor(century) + 1;
 }
 
 console.log(centuryFromYear(1905));
