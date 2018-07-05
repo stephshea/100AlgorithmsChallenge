89aafac9382f6699a4337671a39a2965911ab991
Stephanie <stephcshea@gmail.com>
2018-07-05 10:01:48 -0400

    completed shapeArea

--- shapeArea/shapeArea.js
+++ shapeArea/shapeArea.js
@@ -1,4 +1,7 @@
 function shapeArea(n) {
+  // let powed = n-1;
+  let poly = n*n + Math.pow(n-1, 2);
+return poly;
 }
 console.log(shapeArea(2));
 console.log(shapeArea(3));
--- shapeArea/shapeArea.ts
+++ shapeArea/shapeArea.ts
@@ -1,5 +1,7 @@
 function shapeArea(n: number): number {
-
+  let powed = n-1;
+  let poly = n*n + Math.pow(powed, 2);
+return poly;
 }
 
 console.log(shapeArea(2));
