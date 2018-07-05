06c2845f84e2ef2c8f75ce2d54a61fd6cd6ced1d
Stephanie <stephcshea@gmail.com>
2018-07-05 08:46:11 -0400

    completed adjacentElements

--- adjacentElementsProduct/adjacentElementsProduct.js
+++ adjacentElementsProduct/adjacentElementsProduct.js
@@ -1,4 +1,15 @@
 function adjacentElementsProduct(inputArray) {
-   
+  let product = 0;
+  let highest = inputArray[0] * inputArray[1];
+  
+  for(let i = 1; i <  inputArray.length; i++){
+    product = inputArray[i] * inputArray[i+1];
+      if(product > highest) {
+        highest = product;    
+      }
+    } 
+  return highest;
 }
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+
+console.log(adjacentElementsProduct([-23, 4, -3, 8, -12]));
