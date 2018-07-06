0260a6475fd3b3b33b8a5fcbbdb31a1c46e569d7
Stephanie <stephcshea@gmail.com>
2018-07-06 09:29:16 -0400

    completed domainTypes

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,21 +1,25 @@
 function domainType(domains: string[]): string[] {
 const domainTypes: string [] = [];
 
+let domainTypes = [];
 for (let i = 0; i < domains.length; i++) {
-  if (domains[i].includes("org")) {
+  const domain = domains[i].split(".");
+  const type = domain[domain.length - 1];
+  if (type === "org") {
     domainTypes.push("organization");
     }
-  if (domains[i].includes("com")) {
+  if (type === "com") {
     domainTypes.push("commercial");
     }
-  if (domains[i].includes("net")) {
+  if (type === "net") {
     domainTypes.push("network");
     }
-  if (domains[i].includes("info")) {
+  if (type === "info") {
     domainTypes.push("information");
     }
   }
-return domainTypes;
+   return domainTypes;
 }
 
 console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(domainType(["com.net.info", "org.com.net", "net.com.org", "info.net.com"]));
