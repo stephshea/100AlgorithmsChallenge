1567aa387a8224b8ca32391a0c25b23aa00236e1
Stephanie <stephcshea@gmail.com>
2018-07-06 07:36:34 -0400

    completed areEqually Strong

--- areEquallyStrong/areEquallyStrong.js
+++ areEquallyStrong/areEquallyStrong.js
@@ -1,5 +1,12 @@
 function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) {
+const yourStrongest =  yourLeft >= yourRight ? yourLeft : yourRight;
+const yourWeakest =  yourLeft <= yourRight ? yourLeft : yourRight;
+const friendsStrongest =  friendsLeft >= friendsRight ? friendsLeft : friendsRight;
+const friendsWeakest =  friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+
+return yourStrongest === friendsStrongest && yourWeakest === friendsWeakest;
+
 }
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,5 +1,9 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-
+  const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
+  const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
+  const friendsStrongest = friendsLeft >= friendsRight ? friendsLeft : friendsRight;
+  const friendsWeakest = friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+  return yourStrongest === friendsStrongest && yourWeakest === friendsWeakest;
 }
 
 // console.log(areEquallyStrong(10, 15, 15, 10))
