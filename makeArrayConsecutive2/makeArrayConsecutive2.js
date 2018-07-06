c7daee402e1b3d5ea2f07620d09061a76e90a62c
Stephanie <stephcshea@gmail.com>
2018-07-06 17:42:50 -0400

    completed makeArrayConsecutive2

--- makeArrayConsecutive2/makeArrayConsecutive2.js
+++ makeArrayConsecutive2/makeArrayConsecutive2.js
@@ -1,3 +1,19 @@
 function makeArrayConsecutive2(statues) {
+  
+  const sortedStatues = statues.sort((a, b) => a - b);
+  let firstNum = sortedStatues[0];
+  let lastNum = sortedStatues[sortedStatues.length-1];
+  let count = 0;
+
+  for(var i = firstNum; i < lastNum; i++) {
+    if(!statues.includes(i)) {
+      count++;
+    }
+  }
+  return count;
 }
 console.log(makeArrayConsecutive2([6, 2, 3, 8]));
+console.log(makeArrayConsecutive2([6, 3]));
+console.log(makeArrayConsecutive2([3]));
+console.log(makeArrayConsecutive2([6, 2, 3, 10, 8]));
+console.log(makeArrayConsecutive2([2, 3, 4]));
--- makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,14 @@
 function makeArrayConsecutive2(statues: number[]): number {
+  const sortedStatues = statues.sort((a, b) => a - b);
+  const firstNum = sortedStatues[0];
+  const lastNum = sortedStatues[sortedStatues.length - 1];
+  let count = 0;
 
+  for (let i = firstNum; i < lastNum; i++) {
+    if (!statues.includes(i)) {
+      count++;
+    }
+  }
+  return count;
 }
-
 console.log(makeArrayConsecutive2([6, 2, 3, 8]));
