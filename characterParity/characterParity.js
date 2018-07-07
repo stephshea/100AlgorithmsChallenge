f6ebfefd255068fcc6881e426deff62c81b5fb48
Stephanie <stephcshea@gmail.com>
2018-07-06 23:31:51 -0400

    characterParity

--- characterParity/characterParity.js
+++ characterParity/characterParity.js
@@ -1,5 +1,18 @@
 function characterParity(symbol) {
+  const sym = parseInt(symbol, 10);
+  return isNaN(sym) ? "not a digit" : sym % 2 === 0 ? "even" : "odd";
+  // let sym = parseInt(symbol); 
+  // if (isNaN(sym)) {
+  // return "not a digit";
+  //   }
+  // else if (sym %2 === 0) {
+  //   return "even";
+  // }
+  // else if( sym % 2 !== 0) {
+  //   return "odd";
+  // }
 }
+console.log(characterParity('b'));
 console.log(characterParity('5'));
 console.log(characterParity('8'));
-console.log(characterParity('q'));
+
--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,5 +1,6 @@
 function characterParity(symbol: string): string {
-   
+  const sym = parseInt(symbol, 10);
+  return isNaN(sym) ? "not a digit" : sym % 2 === 0 ? "even" : "odd";
 }
 
 console.log(characterParity('5'))
