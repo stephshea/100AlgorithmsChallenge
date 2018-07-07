36a634cf9c8a4a175dd0866d92d607e3980ede4d
Stephanie <stephcshea@gmail.com>
2018-07-07 10:10:15 -0400

    completed commonCharacters

--- commonCharacterCount/commonCharacterCount.js
+++ commonCharacterCount/commonCharacterCount.js
@@ -1,13 +1,24 @@
 function commonCharacterCount(s1, s2) {
-let wow =  s1.split("");
-let news = s2.split("");
-  let count = 0;
-  for(let i = 0; i < wow.length; i ++) {
-    if (wow[i] === news[i]) {
-      count++;
-    }
-    
+let likes = [];
+for(let i = 0; i < s1.length; i++) {
+    for(let j = 0; j < s2.length; j++) {
+      if(s1.charAt(i) === s2.charAt(j)) {
+        likes.push(s2.charAt(j));
+      }
+    } 
   }
-  return count;
-}
-console.log(commonCharacterCount('aabcc', 'abcaa'));
+ 
+  return likes.filter(function(i, j) { 
+    return likes.indexOf(i) === j;
+    }).length;
+  // .filter(function(a,b) {
+  //   return b ==likes.lastIndexOf(a);
+  // });
+
+
+  // function countUnique(likes) {
+  //   return new Set(likes).size;
+
+  }
+
+console.log(commonCharacterCount('aadcbec', 'aabedca'));
--- commonCharacterCount/commonCharacterCount.ts
+++ commonCharacterCount/commonCharacterCount.ts
@@ -1,5 +1,14 @@
-function commonCharacterCount(s1: string, s2: string): number); {
-   
-}
-
+function commonCharacterCount(s1: string, s2: string): number {
+    const likes = [];
+    for (let i = 0; i < s1.length; i++) {
+      for (let j = 0; j < s2.length; j++) {
+        if (s1.charAt(i) === s2.charAt(j)) {
+          likes.push(s2.charAt(j));
+        }
+      }
+    }
+    return likes.filter(function(i, j) { 
+      return likes.indexOf(i) === j;
+      }).length;
+    }
 console.log(commonCharacterCount('aabcc', 'adcaa'));
