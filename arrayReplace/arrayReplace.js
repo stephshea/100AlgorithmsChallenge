e61b4672100aab65cb5817dd92cf54ee1bf03bff
Stephanie <stephcshea@gmail.com>
2018-07-06 22:33:05 -0400

    completed arrayReplace

--- arrayReplace/arrayReplace.js
+++ arrayReplace/arrayReplace.js
@@ -1,4 +1,13 @@
 function arrayReplace(inputArray, elemToReplace, substitutionElem) {
-    
-}
+let newArray = [];
+  for(let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] === elemToReplace) {  
+      newArray.push(substitutionElem);
+    }
+    else {
+      newArray.push(inputArray[i]);
+    }
+  }
+  return newArray;
+}  
 console.log(arrayReplace([1, 2, 1], 1, 3));
