7edec9ed547778a82d75e9e16835f82ff661abcf
Stephanie <stephcshea@gmail.com>
2018-07-08 11:30:17 -0400

    conpleted containsDuplicates

--- containsDuplicates/containsDuplicates.js
+++ containsDuplicates/containsDuplicates.js
@@ -1,10 +1,13 @@
 function containsDuplicates(a) {
-  for (let i = 0;  i < a.length; i ++) {
-    if(a[i] === a[i + 1]) {
-    return true;
-    }
-  }
-    return false;
+  return a.length !== new Set(a).size;
+  
+  // a = a.sort((a, b) => a - b);
+  // for (let i = 0;  i < a.length; i ++) {
+  //   if(a[i] === a[i + 1]) {
+  //   return true;
+  //   }
+  // }
+  //   return false;
 }
 console.log(containsDuplicates([1, 2, 3, 1]));
 console.log(containsDuplicates([3, 1]));
