92abb502097fc59ff983f06464d2ef78961df068
Stephanie <stephcshea@gmail.com>
2018-07-09 17:39:20 -0400

    completed factorializeANumber

--- factorializeANumber/factorializeANumber.js
+++ factorializeANumber/factorializeANumber.js
@@ -1,10 +1,17 @@
 function factorializeANumber(num) {
-        //    num *= (num-1);
-        let total = 1;
-for (let i = num; i > 1; i--){
+let total = 1;
+for (let i = 1; i <= num; i++){
 total *= i;
 }
-    return total;
+return total;
+
+//recursion
+if (num >= 1) {
+return num * factorializeANumber(num-1);
+}
+else {
+return 1;
+}
 }  
 console.log(factorializeANumber(5));
 console.log(factorializeANumber(10));
--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,5 +1,16 @@
 function factorializeANumber(num: number): number {
-   
+let total = 1;
+for (let i = 1; i <= num; i++) {
+total *= i;
+}
+return total;
+
+// recursion
+if (num >= 1) {
+return num * factorializeANumber(num - 1);
+}
+else {
+return 1; }
 }
 
 console.log(factorializeANumber(5));
