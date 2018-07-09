b5996a2fec63d77db14080d2ab8bd461e9ddcf4a
Stephanie <stephcshea@gmail.com>
2018-07-08 22:54:46 -0400

    completed extractEachKth

--- extractEachKth/extractEachKth.js
+++ extractEachKth/extractEachKth.js
@@ -1,3 +1,5 @@
 function extractEachKth(inputArray, k) {
+inputArray.splice(k-1, 1);
+return inputArray;
 }
 console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
--- extractEachKth/extractEachKth.ts
+++ extractEachKth/extractEachKth.ts
@@ -1,4 +1,6 @@
 function extractEachKth(inputArray:number[], k: number): number[] {
+  inputArray.splice(k - 1, 1);
+return inputArray;
 }
 
 console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
