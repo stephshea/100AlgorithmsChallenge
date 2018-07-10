cf176774a4a3f5a4052f781abd5c52b9c467a50f
Stephanie <stephcshea@gmail.com>
2018-07-10 19:10:45 -0400

    completed properNounCorrection

--- properNounCorrection/properNounCorrection.js
+++ properNounCorrection/properNounCorrection.js
@@ -1,4 +1,7 @@
 function properNounCorrection(noun) {
+  
+  return noun.slice(0,1).toUpperCase().concat(noun.slice(1).toLowerCase());
 }
+
 console.log(properNounCorrection('pARiS'));
 console.log(properNounCorrection('John'));
--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,5 +1,5 @@
 function properNounCorrection(noun: string): string {
-   
+  return noun.slice(0, 1).toUpperCase().concat(noun.slice(1).toLowerCase());
 }
 
 console.log(properNounCorrection('pARiS'));
