ad3f4d938d6f242d82a1b2c065854ead75366b88
Stephanie <stephcshea@gmail.com>
2018-07-10 07:42:51 -0400

    completed sortByHeight

--- sortByHeight/sortByHeight.js
+++ sortByHeight/sortByHeight.js
@@ -1,3 +1,15 @@
 function sortByHeight(a) {
+const filteredArray = a.filter((a ) => a !== -1).sort((num1, num2) => num1 - num2);
+console.log(filteredArray);
+
+let indexValue = 0;
+for (let i = 0; i < a.length; i++) {
+    if (a[i] !== -1) {
+      a[i] = filteredArray[indexValue];
+      indexValue++;
+    }
+  }
+  return a;
 }
+
 console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
--- sortByHeight/sortByHeight.ts
+++ sortByHeight/sortByHeight.ts
@@ -1,5 +1,14 @@
 function sortByHeight(a: number[]): number[] {
+  const filteredArray = a.filter((num) => num !== -1).sort((num1, num2)  =>  num1 - num2);
 
+  let indexValue = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== -1) {
+      a[i] = filteredArray[indexValue];
+      indexValue++;
+    }
+  }
+  return a;
 }
 
 console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
