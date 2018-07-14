eaef7acd6c2f33350cacad04fd55bcce0ebd858c
Stephanie <stephcshea@gmail.com>
2018-07-14 10:08:12 -0400

    completed largestOfFour

--- largestOfFour/largestOfFour.js
+++ largestOfFour/largestOfFour.js
@@ -1,4 +1,17 @@
 function largestOfFour(nums) {
+  let max = 0;
+  let largeNums = [];
+  for (let i =0; i < nums.length; i++) {
+    for (let j = 0; j < nums[i].length; j++){
+      if(nums[i][j] > max) {
+        max = nums[i][j];
+      }
+    }
+    largeNums.push(max);
+    max = 0;
+  }
+  return largeNums;
 }
+
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
 console.log(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]));
--- largestOfFour/largestOfFour.ts
+++ largestOfFour/largestOfFour.ts
@@ -1,5 +1,13 @@
 function largestOfFour(nums: number[][]): number[] {
-
+  const largeNums: number [] = [];
+  for (let numberArray of nums) {
+    let max = 0;
+    for (let number of numberArray) {
+      max = max < number ? number : max;
+    }
+    largeNums.push(max);
+  }
+  return largeNums;
 }
 
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) );
