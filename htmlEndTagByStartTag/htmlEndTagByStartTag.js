220fcb13f82e1443725ca0fcbed8aa21cebe9f29
Stephanie <stephcshea@gmail.com>
2018-07-14 19:53:11 -0400

    compleyed htmlEndTagByStartTag

--- htmlEndTagByStartTag/htmlEndTagByStartTag.js
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.js
@@ -1,4 +1,5 @@
 function htmlEndTagByStartTag(startTag) {
+  return startTag.includes("<button type='button' disabled>") ? "</button" : "</i>";
 }
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
 console.log(htmlEndTagByStartTag('<i>'));
--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,5 +1,5 @@
 function htmlEndTagByStartTag(startTag: string): string {
-
+  return startTag.includes("<button type='button' disabled>") ? "</button" : "</i>";
 }
 
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
