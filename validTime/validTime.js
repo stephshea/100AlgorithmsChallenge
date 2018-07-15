a9fe323982929da25fb2025a2462a80e4832c3c9
Stephanie <stephcshea@gmail.com>
2018-07-14 21:55:10 -0400

    completed validTime

--- validTime/validTime.js
+++ validTime/validTime.js
@@ -1,5 +1,17 @@
 function validTime(time) {
+  time.split("");
+for (let i = 0; i < time.length; i++){
+    if (time[0] > 2 || time[1] > 3) {
+      return false;
+    }
+    else if (time[3]>5) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
 }
-console.log(validTime('13:58'));
-console.log(validTime('25:51'));
+console.log(validTime('13:38'));
+console.log(validTime('23:51'));
 console.log(validTime('02:76'));
