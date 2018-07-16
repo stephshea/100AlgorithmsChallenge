2eca8b107121fdb7c3260d098e115c70df32babe
Stephanie <stephcshea@gmail.com>
2018-07-16 07:19:00 -0400

    updating

--- areSimilar/areSimilar.js
+++ areSimilar/areSimilar.js
@@ -1,12 +1,14 @@
 function areSimilar(a, b) {
   for(let i = 0; i < a.length; i++) {
-    // for(let j = 0; j < b.length; j++) {
-    //   if(a.indexOf([i]) === b.indexOf([j])) {
+    for(let j = 0; j < b.length; j++) {
+      if(a.indexOf([i]) === b.indexOf([j])) {
         return a.toString(i) === b.toString(i);
-      }  
+      } 
+    } 
     return false;
   }
-
+  
+}
 console.log(areSimilar([1, 2, 3], [1, 2, 3]));
 console.log(areSimilar([1, 2, 3], [3, 2, 1]));
 console.log(areSimilar([1, 2, 3], [2, 1, 3]));
--- arrayChange/arrayChange.js
+++ arrayChange/arrayChange.js
@@ -1,4 +1,10 @@
 function arrayChange(inputArray) {
-   
+   for (let i = 0; i < inputArray.length; i++) {
+     if (inputArray[i]+ 1 < inputArray[i]) {
+       inputArray[i] + 1;
+     }
+     
+   }
+ 
 }
 console.log(arrayChange([1, 1, 1]));
