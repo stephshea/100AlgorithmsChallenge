83cd4532eda606405ff5d6888f9532e3d227a0eb
Stephanie <stephcshea@gmail.com>
2018-07-20 18:35:09 -0400

    refactored htmlEndTagByStartTag

--- htmlEndTagByStartTag/htmlEndTagByStartTag.js
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.js
@@ -1,5 +1,14 @@
 function htmlEndTagByStartTag(startTag) {
-  return startTag.includes("<button") ? "</button" : "</i>";
+  const tagSplit = startTag.split(" ");
+  const tag = tagSplit[0].toString();
+  let endTag;
+  if (!tag.includes(">")) {
+    endTag = "</" + tag.slice(1) + ">";
+  }
+  else {
+    endTag = "</" + tag.slice(1);
+  }
+  return endTag;
 }
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
 console.log(htmlEndTagByStartTag('<i>'));
--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,6 +1,15 @@
 function htmlEndTagByStartTag(startTag: string): string {
-  return startTag.includes("<button") ? "</button" : "</i>";
+  const tagSplit = startTag.split(" ");
+  const tag = tagSplit[0].toString();
+  let endTag;
+  if (!tag.includes(">")) {
+    endTag = "</" + tag.slice(1) + ">";
+  }
+  else {
+    endTag = "</" + tag.slice(1);
+  }
+  return endTag;
 }
-
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
 console.log(htmlEndTagByStartTag('<i>'))
+console.log(htmlEndTagByStartTag('<i2>'))
