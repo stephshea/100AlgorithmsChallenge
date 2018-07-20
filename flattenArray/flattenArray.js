2209e6954ab11695fbb35665e610175af03c6c45
Stephanie <stephcshea@gmail.com>
2018-07-20 10:26:32 -0400

    completed flattenArray

--- flattenArray/flattenArray.js
+++ flattenArray/flattenArray.js
@@ -1,5 +1,14 @@
 function flattenArray(arr) {
-    
+  let flattened = [];
+  for (let i = 0; i < arr.length; i++) {
+    if(Array.isArray(arr[i])) {
+      flattened = flattened.concat(flattenArray(arr[i]));
+    }
+    else {
+      flattened.push(arr[i]);
+    }
+  }
+  return flattened;  
 }
 console.log(flattenArray([[["a"]], [["b"]]]));
 console.log(flattenArray([1, [2], [3, [[4]]]]));
--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,5 +1,17 @@
 function flattenArray(arr: any[]): any[] {
-   
+  const flattened = [];
+  flatten(arr);
+  function flatten(arr) {
+    arr.forEach((item) => {
+      if (Array.isArray(item)) {
+        flatten(item);
+      }
+      else {
+        flattened.push(item);
+      }
+    });
+  }
+  return flattened;
 }
 
 console.log(flattenArray([[["a"]], [["b"]]]));
