8ea183718f4702f5b5d88a7a25fe1636d929f908
Stephanie <stephcshea@gmail.com>
2018-07-22 16:10:53 -0400

    completed areSimilar

--- areSimilar/areSimilar.js
+++ areSimilar/areSimilar.js
@@ -1,16 +1,33 @@
 function areSimilar(a, b) {
-  for(let i = 0; i < a.length; i++) {
-    for(let j = 0; j < b.length; j++) {
-      if(a.indexOf([i]) === b.indexOf([j])) {
-        return a.toString(i) === b.toString(i);
-      } 
+  let count = 0;
+  a = a.toString();
+  b = b.toString();
+  const c = [];
+  let d = [];
+
+  console.log(a[0]);
+  console.log(b[0]);
+    if(a === b) {
+      return true;
     } 
-    return false;
+
+    for(let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+      c.push(a[i]);
+      d.push(b[i]);
+      }
+    }
+      d = d.reverse();
+      if (c.length === 2 && c.toString() === d.toString()) {
+        return true;
+      }
+        return false;
   }
-  
-}
+
+   
+
 console.log(areSimilar([1, 2, 3], [1, 2, 3]));
 console.log(areSimilar([1, 2, 3], [3, 2, 1]));
 console.log(areSimilar([1, 2, 3], [2, 1, 3]));
 console.log(areSimilar([1, 2, 2], [2, 1, 1]));
-console.log(areSimilar([1, 2, 3, 8, 4], [1, 2, 4, 3, 7]));
+console.log(areSimilar([1, 2, 3, 7, 4], [1, 2, 4, 3, 7]));
--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,5 +1,23 @@
 function areSimilar(a: number[], b: number[]): boolean {
+  a = a.toString();
+  b = b.toString();
+  const c = [];
+  let d = [];
 
+  if( a === b ) {
+      return true;
+  }
+  for(let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+    c.push(a[i]);
+    d.push(b[i]);
+    }
+  }
+  d = d.reverse();
+  if (c.length === 2 && c.toString() === d.toString()) {
+    return true;
+  }
+  return false;
 }
 
 console.log(areSimilar([1, 2, 3], [1, 2, 3]));
