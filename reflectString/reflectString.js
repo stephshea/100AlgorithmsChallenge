b250368d31fbdca459897e353f88129a6013e3aa
Stephanie <stephcshea@gmail.com>
2018-07-24 08:01:25 -0400

    updating

--- reflectString/reflectString.js
+++ reflectString/reflectString.js
@@ -1,5 +1,25 @@
 function reflectString(inputString) {
+  let reflect = [];
+  console.log(inputString.charCodeAt(1))
+  let letters = inputString.split("");
+  console.log(letters);
+  console.log(letters[1]);
+  let letter;
+  // for (let i = 0; i < inputString; i++) {
+  //  if (inputString.charCodeAt(i) < 100)
+
+  // letter = inputString.charCodeAt(i) +10;
+  // reflect.push(letter.fromnCharCode);
+  return letters.map((letter) => {
+    // if (letter.charCodeAt() > 110) {
+    //   letter = letter.charCodeAt - 13;
+    //   reflect.push(letter);
+    //   console.log(reflect);
+    // }
+      return letter.charCodeAt();
+  });
   
+  return reflect.join("");
 }
-//mznv
+//zmnv
 console.log(reflectString("name"));
