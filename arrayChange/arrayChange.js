84e872774feeca2698f5a5c63d5380b65df19a7c
Stephanie <stephcshea@gmail.com>
2018-07-23 21:39:16 -0400

    completed arrayChange

--- arrayChange/arrayChange.js
+++ arrayChange/arrayChange.js
@@ -1,10 +1,12 @@
 function arrayChange(inputArray) {
+   let count = 0;
    for (let i = 0; i < inputArray.length; i++) {
-     if (inputArray[i+ 1] < inputArray[i]) {
-       inputArray[i]++;
-     }
-     else return inputArray;
+     if (inputArray[i] >= inputArray[i + 1]) {
+       let difference = inputArray[i] + 1 - inputArray[i + 1];
+       inputArray[i + 1] = inputArray[i] + 1; 
+       count += difference;
+     } 
    }
- return inputArray;
+ return count;
 }
 console.log(arrayChange([1, 1, 1]));
