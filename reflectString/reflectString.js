ce85759be7668be8f8de170f32a4ca3314f509b9
Stephanie <stephcshea@gmail.com>
2018-07-25 08:24:27 -0400

    completed reflectString

--- reflectString/reflectString.js
+++ reflectString/reflectString.js
@@ -1,25 +1,24 @@
 function reflectString(inputString) {
-  let reflect = [];
-  console.log(inputString.charCodeAt(1))
-  let letters = inputString.split("");
-  console.log(letters);
-  console.log(letters[1]);
-  let letter;
-  // for (let i = 0; i < inputString; i++) {
-  //  if (inputString.charCodeAt(i) < 100)
-
-  // letter = inputString.charCodeAt(i) +10;
-  // reflect.push(letter.fromnCharCode);
-  return letters.map((letter) => {
-    // if (letter.charCodeAt() > 110) {
-    //   letter = letter.charCodeAt - 13;
-    //   reflect.push(letter);
-    //   console.log(reflect);
-    // }
-      return letter.charCodeAt();
-  });
-  
-  return reflect.join("");
+  let letters = {
+      a: 'z', b: 'y', c: 'x', d: 'w', e: 'v',
+      f: 'u', g: 't', h: 's', i: 'r', j: 'q',
+      k: 'p', l: 'o', m: 'n', n: 'm', o: 'l',
+      p: 'k', r: 'i', s: 'h', t: 'g', u: 'f',
+      v: 'e', w: 'd', x: 'c', y: 'b', z: 'a'
+  };
+  return inputString.split("").map((letter) => {
+    return letters[letter] || letter;
+  }).join("");
 }
-//zmnv
 console.log(reflectString("name"));
+
+// let reflect = [];
+//   let letters = {
+//     'a': 'z', 'b': 'y', 'c': 'x', 'd': 'w', 'e': 'v', 'm': 'n', 'n':'m'
+// };
+//   let chars = inputString.split("");
+//   reflect = chars.map((letter) => {
+//     return letters[letter] ? letters[letter] : letter;
+//   });
+//   return reflect.join("");
+// }
--- reflectString/reflectString.ts
+++ reflectString/reflectString.ts
@@ -1,5 +1,16 @@
 function reflectString(inputString: string): string {
-  
-}
 
+    const letters = {
+      a: 'z', b: 'y', c: 'x', d: 'w', e: 'v',
+      f: 'u', g: 't', h: 's', i: 'r', j: 'q',
+      k: 'p', l: 'o', m: 'n', n: 'm', o: 'l',
+      p: 'k', r: 'i', s: 'h', t: 'g', u: 'f',
+      v: 'e', w: 'd', x: 'c', y: 'b', z: 'a'
+  };
+    let reflected = "";
+    inputString.split("").forEach((letter) => {
+    reflected += letters[letter];
+    });
+    return reflected;
+}
 console.log(reflectString("name"));
