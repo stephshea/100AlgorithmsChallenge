f31445ca4c54df55e9742a62001886b9ed4efe84
Stephanie <stephcshea@gmail.com>
2018-07-25 20:51:18 -0400

    completed minimalNumberOfCoins

--- minimalNumberOfCoins/minimalNumberOfCoins.js
+++ minimalNumberOfCoins/minimalNumberOfCoins.js
@@ -1,3 +1,19 @@
 function minimalNumberOfCoins(coins, price) {
+  let count = 0;
+  for (let i = 0; i < price; i++) {
+    if (price % 10 === 0) {
+      count++;
+      price = price - 10;
+    }
+    else if (price %2 === 0) {
+      count++;
+      price = price -2;
+    }
+    else if (price%1 === 0) {
+      count++;
+      prince = price - 1;
+    }
+  }
+  return count;
 }
 console.log(minimalNumberOfCoins([1, 2, 10], 28));
--- minimalNumberOfCoins/minimalNumberOfCoins.ts
+++ minimalNumberOfCoins/minimalNumberOfCoins.ts
@@ -1,5 +1,20 @@
 function minimalNumberOfCoins(coins: number[], price: number): number {
-
+  let count = 0;
+  for (let i = 0; i < price; i++) {
+    if (price % 10 === 0) {
+      count++;
+      price = price - 10;
+    }
+    else if (price % 2 === 0) {
+      count++;
+      price = price - 2;
+    }
+    else if (price % 1 === 0) {
+      count++;
+      price = price - 1;
+    }
+  }
+  return count;
 }
 
 console.log(minimalNumberOfCoins([1, 2, 10], 28));
