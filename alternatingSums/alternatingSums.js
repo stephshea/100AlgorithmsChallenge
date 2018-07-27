1a7afbbb8ffda0c3edebc032de20570f0db9942e
Stephanie <stephcshea@gmail.com>
2018-07-27 17:07:08 -0400

    completed alternatingSums

--- alternatingSums/alternatingSums.js
+++ alternatingSums/alternatingSums.js
@@ -1,4 +1,32 @@
 function alternatingSums(a) {
-    
+  let evenTotal = 0;
+  let oddTotal = 0;
+  a.forEach((element, index) => {
+    if (index % 2 === 0) {
+      evenTotal +=  element;
+    }
+    else {
+      oddTotal += element;
+    }
+  });
+  return [evenTotal, oddTotal];
 }
 console.log(alternatingSums([50, 60, 60, 45, 70]));
+
+// let weights = [];
+// let firstGroup = 0;
+// let secondGroup = 0;
+// let i = 0;
+// let j = 1;
+  // while (i < a.length) {
+  //   firstGroup += a[i];
+  //   i += 2;  
+  // }
+  //   weights.push(firstGroup);
+
+  // while (j < a.length) {
+  //   secondGroup += a[j];
+  //   j += 2;
+  // }
+  //   weights.push(secondGroup);
+// return weights;
--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,14 @@
 function alternatingSums(a: number[]): number[] {
-    
+  let evenTotal = 0;
+  let oddTotal = 0;
+  a.forEach((element, index) => {
+    if (index % 2 === 0) {
+      evenTotal +=  element;
+    }
+    else {
+      oddTotal += element;
+    }
+  });
+  return [evenTotal, oddTotal];
 }
-
 console.log(alternatingSums([50, 60, 60, 45, 70]))
