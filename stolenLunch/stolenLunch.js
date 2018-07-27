c4fac8ad7d857a9cb415665701df4b6b6dc48634
Stephanie <stephcshea@gmail.com>
2018-07-27 18:37:08 -0400

    completed stolenLunch

--- stolenLunch/stolenLunch.js
+++ stolenLunch/stolenLunch.js
@@ -1,3 +1,12 @@
 function stolenLunch(note) {
+  const chars = {
+    0: "a", 1: "b", 2: "c", 3: "d", 4: "e",
+    5: "f", 6: "g", 7: "h", 8: "i", 9: "j",
+    a: "0", b: "1", c: "2", d: "3", e: "4",
+    f: "5", g: "6", h: "7", i: "8", j: "9" 
+  };
+  return note.split("").map((char) => {
+    return chars[char] || char;
+  }).join("");
 }
 console.log(stolenLunch("you'll n4v4r 6u4ss 8t: cdja"));
