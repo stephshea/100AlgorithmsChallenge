09fea2e8bb81fa63d2900afe7f8ec9a639a6699b
Stephanie <stephcshea@gmail.com>
2018-07-27 17:17:35 -0400

    completed arrayMaxConsecutiveSum

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.js
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.js
@@ -1,12 +1,12 @@
 function arrayMaxConsecutiveSum(inputArray, k) {
   let max = 0;
   let pairTotal = 0;
-  for (let i = 0; i < inputArray.length; i++) {
-    (pairTotal = inputArray[i] + inputArray[i + 1])
+  for (let i = 0; i < k; i++) {
+    (pairTotal = inputArray[i] + inputArray[i + 1]);
     if( pairTotal > max) {
       max = pairTotal;
     }
   }
   return max;
 }
-console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 4));
--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,12 @@
 function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
-
+  let max = 0;
+  let pairTotal = 0;
+  for (let i = 0; i < k; i++) {
+    (pairTotal = inputArray[i] + inputArray[i + 1]);
+    if (pairTotal > max) {
+      max = pairTotal;
+    }
+  }
+  return max;
 }
-
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
