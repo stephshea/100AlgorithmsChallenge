f951c2d009e45c7bb592c34d0ff339925ba25fb9
Stephanie <stephcshea@gmail.com>
2018-08-01 08:16:47 -0400

    updating

--- arrayConversion/arrayConversion.js
+++ arrayConversion/arrayConversion.js
@@ -1,3 +1,19 @@
 function arrayConversion(inputArray) {
+  let num;
+  let pairs = [];
+  let multpairs = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    num = inputArray[i] + inputArray[i + 1];
+    pairs.push(num);
+    i++;
+  }
+  for (let j = 0; j < pairs.length; j++) {
+    let num2 = pairs[j] * pairs[j + 1];
+    multpairs.push(num2);
+    j++;
+  }
+  return multpairs.reduce((a, b) => {
+    return a + b;
+  });
 }
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
--- missingLetters/missingLetters.js
+++ missingLetters/missingLetters.js
@@ -1,12 +1,5 @@
 function missingLetters(str) {
-    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    var chars = str.split('');
-    for (var i = 0; i < chars.length; i++) {
-        if (chars[i] !== alphabet[i]) {
-            return alphabet[i];
-        }
-    }
-    return undefined;
+   
 }
 console.log(missingLetters("bce"));
 console.log(missingLetters("abce"));
