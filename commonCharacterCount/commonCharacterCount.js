baef7f64c3b69294f685108691a4cae04b5d9199
Stephanie <stephcshea@gmail.com>
2018-08-03 08:12:21 -0400

    corrected commonCharacterCount

--- commonCharacterCount/commonCharacterCount.js
+++ commonCharacterCount/commonCharacterCount.js
@@ -1,24 +1,40 @@
 function commonCharacterCount(s1, s2) {
-let likes = [];
-for(let i = 0; i < s1.length; i++) {
-    for(let j = 0; j < s2.length; j++) {
-      if(s1.charAt(i) === s2.charAt(j)) {
-        likes.push(s2.charAt(j));
+    s1 = s1.split("");
+    s2 = s2.split("");
+    
+    let s1Obj = {};
+    let s2Obj = {};
+    
+    for (let i =0; i < s1.length; i++) {
+        if (s1Obj.hasOwnProperty(s1[i]) === false) {
+            s1Obj[s1[i]] = 1;
+        }
+        else {
+            s1Obj[s1[i]]++;
+        }
+    }
+    
+    for (let i =0; i < s2.length; i++) {
+        if(s2Obj.hasOwnProperty(s2[i]) === false) {
+            s2Obj[s2[i]] = 1;
+        }
+        else {
+            s2Obj[s2[i]]++;
+        }
+    }
+  
+    let total = 0;
+    for (const prop in s1Obj){
+      if (s2Obj.hasOwnProperty(prop) === true) {
+        if (s2Obj[prop] < s1Obj[prop]){
+          total += s2Obj[prop];
+        }
+        else {
+          total += s1Obj[prop];
+        }
       }
-    } 
+    }
+    return total;
   }
- 
-  return likes.filter((i, j) => { 
-    return likes.indexOf(i) === j;
-    }).length;
-  // .filter(function(a,b) {
-  //   return b ==likes.lastIndexOf(a);
-  // });
-
-
-  // function countUnique(likes) {
-  //   return new Set(likes).size;
-
-  }
-
-console.log(commonCharacterCount('aadcbec', 'aabedca'));
+  console.log(commonCharacterCount('aabcc', 'adcaa'));
+  console.log(commonCharacterCount('zzzz', 'zzzzzz'));
--- commonCharacterCount/commonCharacterCount.ts
+++ commonCharacterCount/commonCharacterCount.ts
@@ -1,14 +1,40 @@
 function commonCharacterCount(s1: string, s2: string): number {
-    const likes = [];
-    for (let i = 0; i < s1.length; i++) {
-      for (let j = 0; j < s2.length; j++) {
-        if (s1.charAt(i) === s2.charAt(j)) {
-          likes.push(s2.charAt(j));
-        }
+  s1 = s1.split("");
+  s2 = s2.split("");
+  
+  const s1Obj = {};
+  const s2Obj = {};
+  
+  for (let i =0; i < s1.length; i++) {
+      if (s1Obj.hasOwnProperty(s1[i]) === false) {
+          s1Obj[s1[i]] = 1;
+      }
+      else {
+          s1Obj[s1[i]]++;
+      }
+  }
+  
+  for (let i =0; i < s2.length; i++) {
+      if(s2Obj.hasOwnProperty(s2[i]) === false) {
+          s2Obj[s2[i]] = 1;
+      }
+      else {
+          s2Obj[s2[i]]++;
+      }
+  }
+
+  let total = 0;
+  for (const prop in s1Obj){
+    if (s2Obj.hasOwnProperty(prop) === true) {
+      if (s2Obj[prop] < s1Obj[prop]){
+        total += s2Obj[prop];
+      }
+      else {
+        total += s1Obj[prop];
       }
     }
-    return likes.filter((i, j) => {
-      return likes.indexOf(i) === j;
-      }).length;
-    }
+  }
+  return total;
+}
 console.log(commonCharacterCount('aabcc', 'adcaa'));
+console.log(commonCharacterCount('zzzz', 'zzzzzz'));
