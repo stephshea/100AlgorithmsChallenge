d8963bf7735809f64cb08e88089570db26cc4326
Stephanie <stephcshea@gmail.com>
2018-08-03 18:04:16 -0400

    completed sumOfTwo

--- sumOfTwo/sumOfTwo.js
+++ sumOfTwo/sumOfTwo.js
@@ -1,11 +1,13 @@
 function sumOfTwo(a, b, v) {
-  for (let i = 0; i < b.length; i++) {
-    // for (let j = i; j < b.length; j++) {
-      if (a[i] + b[i] === v){
-        return true;
-      } 
+  let diff = [];
+  for (let num of b) {
+    diff.push(v - num);
+  }
+  for (let num of a) {
+    if(diff.includes(num)) {
+      return true;
     }
+  }
   return false;
-  
 }
 console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
--- sumOfTwo/sumOfTwo.ts
+++ sumOfTwo/sumOfTwo.ts
@@ -1,5 +1,17 @@
 function sumOfTwo(a: number[], b: number[], v: number): boolean {
+  const hashMap = {};
 
+  for (let num of a) {
+    const difference = v - num;
+    hashMap[difference] = difference;
+  }
+  console.log(hashMap);
+  for (let num of b) {
+    if (hashMap.hasOwnProperty(num)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
