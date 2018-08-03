041c3487e768c32468830ee6de57cf0634bb6408
Stephanie <stephcshea@gmail.com>
2018-08-02 22:04:41 -0400

    completed isLucky

--- isLucky/isLucky.js
+++ isLucky/isLucky.js
@@ -1,4 +1,19 @@
 function isLucky(n) {
+  n = n.toString().split('');
+  let divide = n.length / 2;
+  let left = n.splice(0, divide);
+  let right = n.slice(0, divide);
+  left = left.reduce((a, b) => {
+    return parseInt(a) + parseInt(b);
+  });
+  right = right.reduce((a, b) => {
+    return parseInt(a, 10) + parseInt(b, 10);
+  });
+  if (left === right) {
+    return true;
+  }
+  return false;
+    
 }
 console.log(isLucky(1230));
 console.log(isLucky(239017));
