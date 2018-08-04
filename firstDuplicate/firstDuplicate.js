6a9d1b828cb430fe1bdeb368f65722c568422a3e
Stephanie <stephcshea@gmail.com>
2018-08-04 08:32:03 -0400

    completed firstDuplicate

--- firstDuplicate/firstDuplicate.js
+++ firstDuplicate/firstDuplicate.js
@@ -1,4 +1,14 @@
 function firstDuplicate(a) {
+  let dups = [];
+  for (let i = 0; i < a.length; i++) {
+    if (!dups.includes(a[i])) {
+      dups.push(a[i]);
+    }
+    else if (dups.includes(a[i])) {
+      return a[i];
+    }
+  }
+  return -1;
 }
-console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
+console.log(firstDuplicate([2, 5, 1, 3, 5, 3, 2]));
 console.log(firstDuplicate([2, 4, 3, 5, 1]));
--- firstDuplicate/firstDuplicate.ts
+++ firstDuplicate/firstDuplicate.ts
@@ -1,6 +1,13 @@
 function firstDuplicate(a: number[]): number {
+  const firstDup = {};
 
+  for (let num of a) {
+    if (firstDup.hasOwnProperty(num)) {
+      return num;
+    }
+    firstDup[num] = num;
+  }
+  return -1;
 }
-
 console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
 console.log(firstDuplicate([2, 4, 3, 5, 1]));
