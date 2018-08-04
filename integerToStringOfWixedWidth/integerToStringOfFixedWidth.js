dee6297d92e8f202aebdb0bc56b2800f27c4f1eb
Stephanie <stephcshea@gmail.com>
2018-08-03 22:09:09 -0400

    completed integerToStringOfFixedWidth

--- integerToStringOfWixedWidth/integerToStringOfFixedWidth.js
+++ integerToStringOfWixedWidth/integerToStringOfFixedWidth.js
@@ -1,5 +1,16 @@
 function integerToStringOfFixedWidth(number, width) {
+  number = number.toString();
+  if (number.length > width) {
+    return number.slice(width);
+  }
+  else if (number.length < width) {
+    return "0".repeat(width - number.length) + number;
+  } else {
+    return number;
+  }
 }
+
+
 console.log(integerToStringOfFixedWidth(1234, 2));
 console.log(integerToStringOfFixedWidth(1234, 4));
 console.log(integerToStringOfFixedWidth(1234, 5));
--- integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
@@ -1,5 +1,14 @@
 function integerToStringOfFixedWidth(number: number, width: number): string {
-  
+  number = number.toString();
+  const length = number.length;
+  if (length > width) {
+    return number.slice(width);
+  }
+  else if (length < width) {
+    return "0".repeat(width - length) + number;
+  } else {
+    return number;
+  }
 }
 
 console.log(integerToStringOfFixedWidth(1234, 2));
