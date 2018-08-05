a4a5e0eb1c884a1ddd23c47b8fecf356ffee4a28
Stephanie <stephcshea@gmail.com>
2018-08-05 09:14:02 -0400

    completed fareEstimator

--- fareEstimator/fareEstimator.js
+++ fareEstimator/fareEstimator.js
@@ -1,8 +1,22 @@
 function fareEstimator(ride_time, ride_distance, cost_per_minute, cost_per_mile) {
-    var fareCost = [];
-    for (var i = 0; i < cost_per_mile.length; i++) {
-        fareCost.push(ride_time * cost_per_minute[i] + ride_distance * cost_per_mile[i]);
-    }
-    return fareCost;
+    const fareEstimates = [];
+    let fare = 0;
+    let miles = 0;
+    const fareMiles = [];
+    const baseFares = [];
+    let baseFare = 0
+    cost_per_minute.forEach((min) => {
+        baseFare = ride_time * min;
+        baseFares.push(baseFare);
+    });
+    cost_per_mile.forEach((mile) => {
+        miles = ride_distance * mile;
+        fareMiles.push(miles);    
+        });
+    for (let i = 0; i < fareMiles.length; i++) {
+        fare = baseFares[i] + fareMiles[i];
+        fareEstimates.push(fare.toFixed(1));
+    }  
+    return fareEstimates;
 }
 console.log(fareEstimator(30, 7, [0.2, 0.35, 0.4, 0.45], [1.1, 1.8, 2.3, 3.5]));
--- fareEstimator/fareEstimator.ts
+++ fareEstimator/fareEstimator.ts
@@ -1,10 +1,9 @@
 function fareEstimator(ride_time: number, ride_distance: number, cost_per_minute: number[], cost_per_mile: number[]): number[] {
-    const fareCost: number[] = [];
-
+    const fareCost: number [] = [];
+    let fare = 0;
     for (let i = 0; i < cost_per_mile.length; i++) {
-        fareCost.push(ride_time * cost_per_minute[i] + ride_distance * cost_per_mile[i]);
+        fareCost.push((ride_time * cost_per_minute[i] + ride_distance * cost_per_mile[i]).toFixed(1));
     }
-
     return fareCost;
 }
 
