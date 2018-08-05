648b36c71a47c21a281cadae3f0eb8559361700c
Stephanie <stephcshea@gmail.com>
2018-08-05 07:34:27 -0400

    completed fancyRide

--- fancyRide/fancyRide.js
+++ fancyRide/fancyRide.js
@@ -1,9 +1,17 @@
 function fancyRide(l, fares) {
-    var rides = ['UberX', 'UberXL', 'UberPlus', 'UberBlack', 'UberSUV'];
-    for (var i = fares.length - 1; i >= 0; i--) {
-        if (fares[i] * l <= 20) {
-            return rides[i];
+    let totals = [];
+    let total;
+    let index = 0;
+    const carTypes =  ["UberX", "UberXL", "UberPlus", "UberBlack", "UberSUV"];
+    fares.forEach((fare) => {
+        total = l * fare;
+        totals.push(total);    
+    });
+    for (let fare of totals) {
+        if(Math.abs(fare) <= 20) {
+            index = totals.indexOf(fare);
         }
     }
+ return carTypes[index];  
 }
 console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
