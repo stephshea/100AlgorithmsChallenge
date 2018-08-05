402fbfd10a21dca1a04d86a2f8e11de1c2f4db8a
Stephanie <stephcshea@gmail.com>
2018-08-05 09:37:16 -0400

    completed findEmailDomain

--- findEmailDomain/findEmailDomain.js
+++ findEmailDomain/findEmailDomain.js
@@ -1,5 +1,11 @@
 function findEmailDomain(address) {
-    
+  const addressSplit = address.split("");
+  var reg = /^[a-z]+$/i;
+  for (let i = 0; i < addressSplit.length; i++) {
+    if (addressSplit[i].includes("@") && reg.test(addressSplit[i + 1])) {
+      return addressSplit.splice(i).join("");   
+    }
+  }
 }
 console.log(findEmailDomain('prettyandsimple@example.com'));
 console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
--- findEmailDomain/findEmailDomain.ts
+++ findEmailDomain/findEmailDomain.ts
@@ -1,5 +1,5 @@
 function findEmailDomain(address: string): string {
-    const lastAtIndex = address.lastIndexOf('@');
+    const lastAtIndex = address.lastIndexOf("@");
 
     return address.slice(lastAtIndex + 1, address.length);
 }
