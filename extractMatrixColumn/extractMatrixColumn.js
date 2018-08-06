8679a85d427c478f4f044c9e2883dfd6deb94848
Stephanie <stephcshea@gmail.com>
2018-08-06 18:21:09 -0400

    completed extractMatrixColumn

--- extractMatrixColumn/extractMatrixColumn.js
+++ extractMatrixColumn/extractMatrixColumn.js
@@ -1,3 +1,8 @@
 function extractMatrixColumn(matrix, column) {
+  let extraction = [];
+  for (let num in matrix) {
+      extraction.push(matrix[num][column]);
+    }
+  return extraction;
 }
 console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,7 @@
 function extractMatrixColumn(matrix: number[][], column: number): number[] {
-
+  const extraction: number [] = [];
+  matrix.forEach((row) => extraction.push(row[column]));
+  return extraction;
 }
 
 console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
