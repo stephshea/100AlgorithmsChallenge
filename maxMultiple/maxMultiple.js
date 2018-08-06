7762137f513ab90ae51a644e25e09541213c6aac
Stephanie <stephcshea@gmail.com>
2018-08-06 18:52:19 -0400

    completed axMultiple

--- maxMultiple/maxMultiple.js
+++ maxMultiple/maxMultiple.js
@@ -1,3 +1,6 @@
 function maxMultiple(divisor, bound) {
+  // return Math.floor(bound/divisor) * divisor;
+  let remainder = bound % divisor;
+  return bound - remainder;
 }
 console.log(maxMultiple(3, 10));
--- maxMultiple/maxMultiple.ts
+++ maxMultiple/maxMultiple.ts
@@ -1,5 +1,6 @@
 function maxMultiple(divisor: number, bound: number): number  {
-
+  // return Math.floor(bound/divisor) * divisor;
+  let remainder = bound % divisor;
+  return bound - remainder;
 }
-
 console.log(maxMultiple(3,10));
