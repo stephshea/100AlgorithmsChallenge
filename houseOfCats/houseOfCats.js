896bbd78342326f9b2af18aed985b4cd4e359c38
Stephanie <stephcshea@gmail.com>
2018-08-05 23:00:42 -0400

    completed houseOfCats

--- houseOfCats/houseOfCats.js
+++ houseOfCats/houseOfCats.js
@@ -1,4 +1,14 @@
 function houseOfCats(legs) {
+  const critters = [];
+  critters.push(Math.floor(legs/4));
+  critters.push(Math.floor(legs/2));
+  critters.forEach((legged) => {
+    if (legged === 0) {
+      critters.shift(legged);
+    } 
+  });
+    return critters;
 }
+
 console.log(houseOfCats(6));
 console.log(houseOfCats(2));
--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,6 +1,10 @@
 function houseOfCats(legs: number): number[] {
-
+  const critters = [];
+  while (legs >= 0) {
+    critters.unshift(legs / 2);
+    legs -= 4;
+  }
+  return critters;
 }
-
 console.log(houseOfCats(6));
 console.log(houseOfCats(2));
