d81f845b78bfbd8ea6e503e600a3634d69be9809
Stephanie <stephcshea@gmail.com>
2018-08-07 07:59:31 -0400

    completed sumAllPrimes

--- sumAllPrimes/sumAllPrimes.js
+++ sumAllPrimes/sumAllPrimes.js
@@ -1,4 +1,19 @@
 function sumAllPrimes(num) {
+  let sum = 0;
+  function isPrime(num) {
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0){
+        return false;
+      }
+    }
+    return true;
+  }
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
 }
 console.log(sumAllPrimes(10));
 console.log(sumAllPrimes(977));
--- sumAllPrimes/sumAllPrimes.ts
+++ sumAllPrimes/sumAllPrimes.ts
@@ -1,5 +1,16 @@
 function sumAllPrimes(num: number): number {
-
+  let primesTotal = 0;
+  for (let i = 2; i <= num; i++) {
+    for (let j = 2; j <= i; j++) {
+      if (i === j) {
+        primesTotal += i;
+      }
+      if (i % j === 0) {
+        break;
+      }
+    }
+  }
+  return primesTotal;
 }
 
 console.log(sumAllPrimes(10));
