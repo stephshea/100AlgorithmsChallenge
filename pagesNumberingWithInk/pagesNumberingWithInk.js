d62469ef72e3b90189544d6d55b89dacb07965c7
Stephanie <stephcshea@gmail.com>
2018-08-08 07:49:01 -0400

    completed pagesNumberingWithInk

--- pagesNumberingWithInk/pagesNumberingWithInk.js
+++ pagesNumberingWithInk/pagesNumberingWithInk.js
@@ -1,9 +1,17 @@
 function pagesNumberingWithInk(current, numberOfDigits) {
-    while (numberOfDigits >= current.toString().length) {
-        numberOfDigits -= current.toString().length;
-        if (numberOfDigits >= current.toString().length) {
-            current++;
-        }
+    let page = 0;
+    const pages = [];
+    let count = 0;
+    for (let i = 0; i < numberOfDigits; i++ ) {
+         page = current ++;
+         count += page.toString().length;
+         if(count <= numberOfDigits){
+         pages.push(page);
+         }
     }
-    return current;
+    return pages.pop();
+ 
 }
+console.log(pagesNumberingWithInk(1, 5));
+console.log(pagesNumberingWithInk(21, 5));
+console.log(pagesNumberingWithInk(8, 4));
--- pagesNumberingWithInk/pagesNumberingWithInk.ts
+++ pagesNumberingWithInk/pagesNumberingWithInk.ts
@@ -1,12 +1,14 @@
 function pagesNumberingWithInk(current: number, numberOfDigits: number): number {
     while (numberOfDigits >= current.toString().length) {
-
         numberOfDigits -= current.toString().length;
 
         if (numberOfDigits >= current.toString().length) {
             current++;
         }
     }
-
     return current;
 }
+
+console.log(pagesNumberingWithInk(1, 5));
+console.log(pagesNumberingWithInk(21, 5));
+console.log(pagesNumberingWithInk(8, 4));
