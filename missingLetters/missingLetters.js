ebf01a7ba4fa640ed7e1cba7276d961bf9f32676
Stephanie <stephcshea@gmail.com>
2018-08-10 08:24:41 -0400

    completed missingLetters

--- missingLetters/missingLetters.js
+++ missingLetters/missingLetters.js
@@ -1,18 +1,14 @@
 function missingLetters(str) {
-  let alphabet = "abcdefghijklmnopqrstuvwxyz";
-  alphabet = alphabet.split("");
-  str = str.split("");
-  let missingLetter = "";
-  for (let i = 0; i < str.length; i++) {
-    if (str[i].indexOf(i) !== str[i + 1].indexOf(i + 1))  {
-     str[i + 1]= alphabet[i];
-     missingLetter = str[i + 1];
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  let letters = str.split("");
+  for (let i = 0; i < letters.length; i++) { 
+    if (letters[i] !== alphabet[i])  {
+     return alphabet[i];
     }
-    return missingLetter;
   }   
   return undefined;
 }
-console.log(missingLetters("bce"));
 console.log(missingLetters("abce"));
+console.log(missingLetters("ace"));
 console.log(missingLetters("abcdefghjklmno"));
 console.log(missingLetters("abcdefghijklmnopqrstuvwxyz"));
--- missingLetters/missingLetters.ts
+++ missingLetters/missingLetters.ts
@@ -1,5 +1,12 @@
 function missingLetters(str: string): string {
-    
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  const letters = str.split("");
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i] !== alphabet[i]) {
+      return alphabet[i];
+    }
+  }
+  return undefined;
 }
 console.log(missingLetters("bce"));
 console.log(missingLetters("abce"));
